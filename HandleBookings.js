33dce1ec23445fc15e850cd4ddc458ac8a1e335d
Zulkifal05 <zulkifalali299@gmail.com>
2025-06-05 17:48:15 +0500

    Train booking page search & display trains logic done!

---
+++ HandleBookings.js
@@ -0,0 +1,136 @@
+class Bookings {
+    constructor() {
+        this.trainPage = document.querySelector(".train-book-page");
+        this.departureSelect = document.querySelector("#departure-select");
+        this.destinationSelect = document.querySelector("#destination-select");
+        this.searchTrainBtn = document.querySelector(".train-search-btn");
+        //Above are neccesary DOM objects
+
+        this.trainBookBtnPressCount = 0;  //Counter for founded trains at  entered destination & departure
+    }
+
+    LoadPage(typeofPage) {  //Function to load train page
+        if(typeofPage === "train") {
+            this.trainPage.style.display = "flex";
+            this.searchTrainBtn.addEventListener("click", () => {
+                this.BookTrain();
+            })
+        }
+    }
+
+    CreateNode(text) {  //For creating span tags as required for founded trains
+        let node = document.createElement("span");
+        node.setAttribute("class","span");
+        node.innerText = `${text}`;
+        return node;
+    }
+
+    TrainInfoAfterSearch(text) {  //For poping information whenever problem with showing trains
+        let emptyLeftPara = document.createElement("p")
+        emptyLeftPara.setAttribute("class","emptyLeftPara");
+        emptyLeftPara.innerText = `${text}`;
+        this.trainPage.append(emptyLeftPara);
+        setTimeout(() => {
+        emptyLeftPara.remove();
+        },1500)
+    }
+
+    BookTrain() {  //Performs functionality after user clicks search train
+        let userDeparture = this.departureSelect.value;
+        let userDestination = this.destinationSelect.value;
+        
+        if(userDeparture === "Empty" || userDestination === "Empty") {  //If user didn't selected city for whether departure or destination or both 
+            if(this.trainBookBtnPressCount > 0) {  //Track if successfull search buttons presses were greater then 0 so delete previously showed data 
+                document.getElementById("remove").remove();
+                this.trainBookBtnPressCount = 0;
+                user.footer.style.position = "absolute";  //Footer's position absolute beacause height of page is less
+            }  //Remove previous all founded trains displayed & reset count to 0 as needed at logic at line 62
+            this.TrainInfoAfterSearch("Please Select Cities!");
+        }
+        else if (userDeparture === userDestination) {  //If user selected same cities for both departure & destination
+            if(this.trainBookBtnPressCount > 0) {  //Track if successfull search buttons presses were greater then 0 so delete previously showed data 
+                document.getElementById("remove").remove();
+                this.trainBookBtnPressCount = 0;
+                user.footer.style.position = "absolute";  //Footer's position absolute beacause height of page is less
+            }  //Remove previous all founded trains displayed & reset count to 0 as needed at logic at line 62
+            this.TrainInfoAfterSearch("Please Select Different Cities!");
+        }
+        else {  //If cities input is without any error
+            let trainArray = JSON.parse(localStorage.getItem("trainTimings"));  //Get traintimings stored in localstorage
+            let trainsFounded = 0;  //To track no of trains founded
+            let foundedTrainContainer = document.createElement("div");  //Div which will show trains
+            foundedTrainContainer.id = "remove";
+
+            if(this.trainBookBtnPressCount > 0) {  //Track if successfull search buttons presses were greater then 0 so delete previously showed data 
+                document.getElementById("remove").remove();
+            }
+            this.trainBookBtnPressCount++;
+
+            for (let i=0 ; i<trainArray.length ; i++) {  //Loop for checking each object stored in array
+                if(userDeparture === trainArray[i].departure && userDestination === trainArray[i].destination) {  //If destination & departure is same as user's in localstorage
+                    let trainFounded = document.createElement("div");
+                    //Created div which will hold one train founded record
+    
+                    let departurePara = document.createElement("p");
+                    departurePara.innerText = ` : ${userDeparture}`;
+                    departurePara.prepend(this.CreateNode("From"));
+                    departurePara.setAttribute("class","train-founded-paras");
+                    //Paragraph for seting From
+    
+                    let destinationPara = document.createElement("p");
+                    destinationPara.innerText = ` : ${userDestination}`;
+                    destinationPara.prepend(this.CreateNode("To"));
+                    destinationPara.setAttribute("class","train-founded-paras");
+                    //Paragraph for seting To
+
+                    let timePara = document.createElement("p");
+                    timePara.innerText = ` : ${trainArray[i].time} Hrs`;
+                    timePara.prepend(this.CreateNode("Time"));
+                    timePara.setAttribute("class","train-founded-paras");
+                    //Paragraph for seting time
+
+                    let datePara = document.createElement("p");
+                    datePara.innerText = ` : ${trainArray[i].date}`;
+                    datePara.prepend(this.CreateNode("Date"));
+                    datePara.setAttribute("class","train-founded-paras");
+                    //Paragraph for seting date
+
+                    let bookBtn = document.createElement("button");
+                    bookBtn.innerText = "Book it"
+                    bookBtn.setAttribute("class","trains-founded-book-btn");
+                    //Create booking button in every founded train div
+
+                    trainFounded.append(departurePara);
+                    trainFounded.append(destinationPara);
+                    trainFounded.append(timePara);
+                    trainFounded.append(datePara);
+                    trainFounded.append(bookBtn);
+                    trainFounded.setAttribute("class","trains-founded");
+                    //Append all the childs (Paragraphs & button) in main div holding one record
+                    foundedTrainContainer.append(trainFounded);  //Append each train record div to main container of all founded trains
+                    trainsFounded++;  //Increment the founded trains count 
+                }
+            }
+            if(trainsFounded > 1) {  //Do footer's position static if more then 1 trains found so UI has no problem
+                user.footer.style.position = "static";
+            }
+            else if (trainsFounded === 0) {  //Display message if no trains are found
+                this.TrainInfoAfterSearch("No Trains Found!");
+            }
+            else {  //If only 1 train is found keep the footer location as absolute
+                user.footer.style.position = "absolute";
+            }
+            foundedTrainContainer.setAttribute("class","foundedTrainContainer");
+            this.trainPage.append(foundedTrainContainer);
+            //Set the class and append container of all the trains founded in main trains page div
+        }
+    }
+}
+  
+let book = new Bookings();  //Object created of Bookings class
+
+//Event listener for trains search button
+book.searchTrainBtn.addEventListener("click", () => {
+    book.BookTrain()
+})
+
--- MainPage.js
+++ MainPage.js
@@ -3,19 +3,14 @@ class MainPage {
         this.trainBookBtn = document.querySelector("#Train-book-btn");
         this.cinemaBookBtn = document.querySelector("#Cinema-book-btn");
         this.hotelBookBtn = document.querySelector("#Hotel-book-btn");
-    }
+    }  //DOM element needed from main page (booking buttons)
 }
 
 let mainPageObj = new MainPage();
 
-mainPageObj.trainBookBtn.addEventListener("click", () => {
-    mainSection.style.display = "none";
-})
-
-mainPageObj.cinemaBookBtn.addEventListener("click", () => {
-    mainSection.style.display = "none";
-})
-
-mainPageObj.hotelBookBtn.addEventListener("click", () => {
-    mainSection.style.display = "none";
+mainPageObj.trainBookBtn.addEventListener("click", () => {  //Event listener for book now of train booking card in main page
+    user.mainSection.style.display = "none";
+    user.footer.style.position = "absolute";  //Beacause height of train page is less
+    book.LoadPage("train");
+    //Display train booking page and display none of previous page
 })
